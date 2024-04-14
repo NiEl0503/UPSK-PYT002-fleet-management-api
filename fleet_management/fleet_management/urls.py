7a3bd669f0255af2887fb36b12ce7434071782d7
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-04-14 19:45:32 -0300

    anadiendo drf_yasg al proyecto para documentar los endpoints

--- fleet_management/fleet_management/urls.py
+++ fleet_management/fleet_management/urls.py
@@ -1,24 +1,24 @@
-"""
-URL configuration for fleet_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""Módulo que define los patrones de URL para una aplicación web."""
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path
 from .views import TaxiList
 
+SchemaView = get_schema_view(
+   openapi.Info(
+      title="Fleet Management",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="ncontreraskanan@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('taxis/', TaxiList.as_view(), name='taxi-list'),
+    path('docs/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
