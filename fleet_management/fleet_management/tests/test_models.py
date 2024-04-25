fb717359e8109939ebae44d46b5d82279662daee
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-04-25 14:18:27 -0300

    criando testes para localização em models.py

--- fleet_management/fleet_management/tests/test_models.py
+++ fleet_management/fleet_management/tests/test_models.py
@@ -1,6 +1,8 @@
 """Módulo que contiene las pruebas para los modelos de la aplicación."""
 from django.test import TestCase
+from django.utils import timezone
 from ..models import Taxi
+from ..serializers import LocationHistory
 
 # pylint: disable=no-member
 class TaxiModelTest(TestCase):
@@ -13,3 +15,15 @@ class TaxiModelTest(TestCase):
         """Obtener el taxi creado en el método setUp"""
         taxi = Taxi.objects.get(plate='DEF987')
         self.assertEqual(taxi.plate, 'DEF987')
+
+class LocationHistoryModelTest(TestCase):
+    """Prueba para el modelo LocationHistory."""
+    def test_location_history_model(self):
+        """Prueba la creación de un historial de localización."""
+        taxi = Taxi.objects.create(plate="DEF456")
+        date = timezone.now()
+        location_history = LocationHistory.objects.create(taxi=taxi, date=date, latitude=40.7128, longitude=-74.0060)
+        self.assertEqual(location_history.taxi_id, taxi.id)
+        self.assertEqual(location_history.date, date)
+        self.assertEqual(location_history.latitude, 40.7128)
+        self.assertEqual(location_history.longitude, -74.0060)
