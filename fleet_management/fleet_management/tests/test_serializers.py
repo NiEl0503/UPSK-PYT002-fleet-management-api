a8416374a9594b239643e03de0fff9dcbecdc63d
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-04-27 12:43:55 -0300

    trabalhando em testes de localização

--- fleet_management/fleet_management/tests/test_serializers.py
+++ fleet_management/fleet_management/tests/test_serializers.py
@@ -1,7 +1,8 @@
 """Módulo que contiene pruebas para los serializadores de la aplicación."""
+from datetime import datetime
 from django.test import TestCase
 from ..models import Taxi
-from ..serializers import TaxiSerializer
+from ..serializers import TaxiSerializer, LocationHistorySerializer
 
 # pylint: disable=no-member
 class TaxiSerializerTest(TestCase):
@@ -11,3 +12,30 @@ class TaxiSerializerTest(TestCase):
         taxi = Taxi.objects.create(plate='DEF987')
         serializer = TaxiSerializer(taxi)
         self.assertEqual(serializer.data['plate'], 'DEF987')
+
+class LocationHistorySerializerTestCase(TestCase):
+    """Caso de prueba para el LocationHistorySerializer."""
+    def setUp(self):
+        self.taxi = Taxi.objects.create(plate="ABC123")
+        self.location_history_data = {
+            "taxi_id": self.taxi.id,
+            "date": datetime.now(),
+            "latitude": 20.7125,
+            "longitude": -54.0065
+        }
+        self.serializer = LocationHistorySerializer(data=self.location_history_data)
+
+    def test_serializer_with_valid_data(self):
+        """Prueba la serialización con datos válidos."""
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_serializer_with_invalid_data(self):
+        """Prueba la serialización con datos inválidos."""
+        invalid_location_history_data = {
+            "taxi_id": 99,
+            "date": "invalid_date_format",
+            "latitude": "invalid_latitude",
+            "longitude": "invalid_longitude"
+        }
+        serializer_with_invalid_data = LocationHistorySerializer(data=invalid_location_history_data)
+        self.assertFalse(serializer_with_invalid_data.is_valid())
--- fleet_management/fleet_management/tests/test_views.py
+++ fleet_management/fleet_management/tests/test_views.py
@@ -1,9 +1,11 @@
 """Módulo que contiene pruebas para las vistas de la aplicación."""
+from datetime import datetime
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APIRequestFactory
 from rest_framework import status
-from ..models import Taxi
+from ..models import Taxi, LocationHistory
+from ..views import LocationHistoryList
 
 # pylint: disable=no-member
 class TaxiListTest(TestCase):
@@ -29,3 +31,24 @@ class TaxiListTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 10)
+
+class LocationHistoryListViewTestCase(TestCase):
+    """Caso de prueba para la vista LocationHistoryList."""
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.taxi = Taxi.objects.create(plate="ABC123")
+        self.location_history = LocationHistory.objects.create(
+            taxi=self.taxi,
+            date=datetime.now(),
+            latitude=90.7168,
+            longitude=-14.0360
+        )
+
+    def test_location_history_list_view(self):
+        """Realizando solicitud GET simulada a la vista LocationHistoryList con un
+        taxi_id y una fecha específica"""
+        request = self.factory.get('/location_history/', {'taxi_id': self.taxi.id, 'date': '2008-02-02'})
+        view = LocationHistoryList.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        
